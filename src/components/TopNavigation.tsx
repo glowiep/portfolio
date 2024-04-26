09eb9397444914f689fa63e05789260f8d37f3e9
Gloria Lim <glorialimartt@gmail.com>
2024-04-26 17:54:48 -0400

    Add TopNavigation component

---
+++ src/components/TopNavigation.tsx
@@ -0,0 +1,16 @@
+
+
+function TopNavigation() {
+  return (
+    <nav>
+      <ul>
+        <li>About Me</li>
+        <li>Work</li>
+        <li>Projects</li>
+        <li>Life</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default TopNavigation;

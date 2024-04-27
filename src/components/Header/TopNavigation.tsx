db8e852f0dd5523a3a59438b78d95fcb63bf827c
Gloria Lim <glorialimartt@gmail.com>
2024-04-26 21:54:36 -0400

    Show navbar background on scroll

--- src/components/Header/TopNavigation.tsx
+++ src/components/Header/TopNavigation.tsx
@@ -1,11 +1,31 @@
 
-import { Flex, Button, useColorMode } from "@chakra-ui/react";
+import { Flex, Button } from "@chakra-ui/react";
+import { useState, useEffect } from 'react';
 import "../../styles/components/TopNavigation.css"
 function TopNavigation() {
-  const { colorMode } = useColorMode();
+  const [showNavBg, setShowNavBg] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      if (scrollY > 10) {
+        setShowNavBg(true);
+      } else {
+        setShowNavBg(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const buttonStyles = {
     variant: "ghost",
     borderRadius: "4em",
+    px: { base: "1em", md: "2em" },
+    gap: { base: "0.5em", md: "1em" },
     // color: "blue.500", 
     _hover: {
       bg: "gray.100", // Example: Change background color on hover
@@ -24,12 +44,13 @@ function TopNavigation() {
       transform="translateX(-50%)" // Adjust for centering
       zIndex="sticky"
       justify="center"
-      backdropFilter="blur(20px)" // Blur effect
+      backdropFilter="blur(7px)" // Blur effect
       // bg="rgba(100, 100, 100, 0.8)" // Glassy background with transparency
-      bg={colorMode === "dark" ? "#2D3748" : "rgba(255, 255, 255, 0.8)"} // Adjust background based on color mode
+      bg={showNavBg === true && "rgba(120, 120, 120, 0.1)"} // Adjust background based on color mode
       py={{ base: "0.5em", md: "1em" }} // Responsive vertical padding
       px={{ base: "0em", md: "3em" }} // Responsive horizontal padding
-      boxShadow={colorMode === "dark" ? "2px 2px 2px rgba(100, 100, 100, 1)" : "2px 2px 5px rgba(0, 0, 0, 0.1)"} // Optional shadow
+      boxShadow={showNavBg === true ? "2px 2px 2px rgba(100, 100, 100, 1)" : "2px 2px 2px rgba(100, 100, 100, 0)"} // Optional shadow
+      outline={showNavBg === true ? "solid rgba(92, 101, 116, 0.8)" : "2px 2px 2px rgba(100, 100, 100, 0)"} // Optional shadow
       borderRadius="4em"
       gap={{ base: "0em", md: "1em" }}
       maxW="screen-xl" // Limit maximum width

62132654d0ef54dda46175ca5c194652d37844e9
Gloria Lim <glorialimartt@gmail.com>
2024-04-27 23:43:55 -0400

    Add seciton tag

--- src/components/Header/TopNavigation.tsx
+++ src/components/Header/TopNavigation.tsx
@@ -35,32 +35,33 @@ function TopNavigation() {
   }
 
   return (
-    <Flex
-      as="nav"
-      // className="top-nav"
-      position="fixed"
-      top="1em"
-      left="50%"
-      transform="translateX(-50%)" // Adjust for centering
-      zIndex="sticky"
-      justify="center"
-      backdropFilter="blur(7px)" // Blur effect
-      // bg="rgba(100, 100, 100, 0.8)" // Glassy background with transparency
-      bg={showNavBg === true && "rgba(120, 120, 120, 0.1)"} // Adjust background based on color mode
-      py={{ base: "0.5em", md: "1em" }} // Responsive vertical padding
-      px={{ base: "0em", md: "3em" }} // Responsive horizontal padding
-      boxShadow={showNavBg === true ? "2px 2px 2px rgba(100, 100, 100, 1)" : "2px 2px 2px rgba(100, 100, 100, 0)"} // Optional shadow
-      outline={showNavBg === true ? "solid rgba(92, 101, 116, 0.8)" : "2px 2px 2px rgba(100, 100, 100, 0)"} // Optional shadow
-      borderRadius="4em"
-      gap={{ base: "0em", md: "1em" }}
-      maxW="screen-xl" // Limit maximum width
-      minW="20rem" 
-    >
-      <Button {...buttonStyles}>Home</Button>
-      <Button {...buttonStyles}>Work</Button>
-      <Button {...buttonStyles}>Projects</Button>
-      <Button {...buttonStyles}>About Me</Button>
-      </Flex>
+    <section className="top-navigation">
+      <Flex
+        as="nav"
+        position="fixed"
+        top="1em"
+        left="50%"
+        transform="translateX(-50%)" // Adjust for centering
+        zIndex="sticky"
+        justify="center"
+        backdropFilter="blur(7px)" // Blur effect
+        // bg="rgba(100, 100, 100, 0.8)" // Glassy background with transparency
+        bg={showNavBg === true && "rgba(120, 120, 120, 0.1)"} // Adjust background based on color mode
+        py={{ base: "0.5em", md: "1em" }} // Responsive vertical padding
+        px={{ base: "0em", md: "3em" }} // Responsive horizontal padding
+        boxShadow={showNavBg === true ? "2px 2px 2px rgba(100, 100, 100, 1)" : "2px 2px 2px rgba(100, 100, 100, 0)"} // Optional shadow
+        outline={showNavBg === true ? "solid rgba(92, 101, 116, 0.8)" : "2px 2px 2px rgba(100, 100, 100, 0)"} // Optional shadow
+        borderRadius="4em"
+        gap={{ base: "0em", md: "1em" }}
+        maxW="screen-xl" // Limit maximum width
+        minW="20rem" 
+      >
+        <Button {...buttonStyles}>Home</Button>
+        <Button {...buttonStyles}>Work</Button>
+        <Button {...buttonStyles}>Projects</Button>
+        <Button {...buttonStyles}>About Me</Button>
+        </Flex>
+      </section>
   )
 }
 

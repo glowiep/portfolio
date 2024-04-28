609ce2f3854b49383d115c8d01e11aa2e8d72117
Gloria Lim <glorialimartt@gmail.com>
2024-04-28 18:39:06 -0400

    Add gradient background

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -1,7 +1,11 @@
+// import background from "/home/labber/myProjects/portfolio/gloria-lim-portfolio/portfolio/src/assets/images/gradient.svg"
 
 function Home() {
   return(
     <section>
+      <div style={{ backgroundImage: `url(/gradient.svg)`}}>
+
+      </div>
     </section>
   )
 }

e18d6a73bed241850d389b8b7415754487c4e662
Gloria Lim <glorialimartt@gmail.com>
2024-04-29 11:06:35 -0400

    Update delay

--- src/components/Pages/Home/Landing.jsx
+++ src/components/Pages/Home/Landing.jsx
@@ -73,7 +73,7 @@ function Landing() {
           <motion.h1
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.6 }}
+            transition={{ delay: 0.5 }}
             style={{
               backgroundClip: "text",
               WebkitBackgroundClip: "text",

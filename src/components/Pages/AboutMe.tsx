eab2fe3c4675a829d760a3a943e8ff55a95b236c
Gloria Lim <glorialimartt@gmail.com>
2024-04-29 21:44:43 -0400

    Format with prettier

--- src/components/Pages/AboutMe.tsx
+++ src/components/Pages/AboutMe.tsx
@@ -4,26 +4,19 @@ function AboutMe() {
   return (
     <section>
       <Flex
-      direction="column" // Set flex direction to column
-      justifyContent="center" // Center content vertically
-      alignItems="center" // Center content horizontally
-      h="100vh" // Set height to full viewport height
-    >
-      <Box pt="60px"> 
-
-      <Text
-        bg='white'
-        bgClip='text'
-        fontSize='6xl'
-        fontWeight='extrabold'
+        direction="column" // Set flex direction to column
+        justifyContent="center" // Center content vertically
+        alignItems="center" // Center content horizontally
+        h="100vh" // Set height to full viewport height
       >
-        About Me
-      </Text>
-    </Box>
-    </Flex>
-      
+        <Box pt="60px">
+          <Text bg="white" bgClip="text" fontSize="6xl" fontWeight="extrabold">
+            About Me
+          </Text>
+        </Box>
+      </Flex>
     </section>
-  )
-};
+  );
+}
 
 export default AboutMe;

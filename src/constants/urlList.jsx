12b40a88b09a79075fcd12783822a875f0f20f06
Gloria Lim <glorialimartt@gmail.com>
2024-05-03 14:26:20 -0400

    Add urlList

---
+++ src/constants/urlList.jsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { FaGithub, FaDev, FaLinkedin } from 'react-icons/fa';
+import { GrDocumentDownload } from "react-icons/gr";
+
+export const urlList = {
+  author: {
+    name: 'Muhammad Ahmad',
+    accounts: [
+      {
+        url: 'https://github.com/glowiep',
+        label: 'Github',
+        type: 'gray',
+        icon: <FaGithub size="2em"/>
+      },
+      {
+        url: 'https://linkedin.com/in/glowiep',
+        label: 'LinkedIn',
+        type: 'linkedin',
+        icon: <FaLinkedin size="2em"/>
+      },
+      {
+        url: 'https://dev.to/glowiep',
+        label: 'DEV.to Articles',
+        type: 'gray',
+        icon: <FaDev size="2em"/>
+      },
+      {
+        url: 'https://bit.ly/gloria_lim',
+        label: 'Resume',
+        type: 'gray',
+        icon: <GrDocumentDownload size="2em"/>
+      },
+      
+    ]
+  }
+};

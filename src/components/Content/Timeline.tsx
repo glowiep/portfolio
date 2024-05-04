015dbcd0b883cf039b4b9b22abc8b1b19a07f8e5
Gloria Lim <glorialimartt@gmail.com>
2024-05-04 19:29:00 -0400

    Update timeline order

--- src/components/Content/Timeline.tsx
+++ src/components/Content/Timeline.tsx
@@ -10,18 +10,20 @@ import {
   useBreakpointValue
 } from '@chakra-ui/react';
 
-const milestones = [
+const milestones = [  
   {
     id: 1,
-    date: 'MAY 20, 2017',
-    title: 'First Commit',
-    description: `Ever since I pushed my first commit to Github in May of 2017, part of me had always wanted to be able to build visually appealing applications that appear simple, but abstracts the layer of complexity for the end user. `
+    date: 'APRIL 26, 2024',
+    title: 'Web Development Diploma',
+    description:
+      'This is one of my proudest achievements. Within 8 intense months, I had made incredible progress in my understanding of the important tools, frameworks, and concepts in Web Development. I graduated with the ability to create web applications, and the skills to continue learning independently.'
   },
   {
     id: 2,
-    date: 'JUNE 4, 2020',
-    title: 'University of Toronto, HBSc.',
-    description: `I graduated with Honors Bachelors of Science degree from the University of Toronto in 2020, with a major in Geographic Information Systems and minors in Economics and Business.`
+    date: 'JULY, 2023',
+    title: 'Pursuing Web Development',
+    description:
+      'I took a leap and enrolled myself in Lighthouse Labs Full-Stack Web Development program. On top of my 40-hour work week, I carved out 30+ hours each week to pursued this interest which had always been in the back of my mind.'
   },
   {
     id: 3,
@@ -32,17 +34,15 @@ const milestones = [
   },
   {
     id: 4,
-    date: 'JULY, 2023',
-    title: 'Pursuing Web Development',
-    description:
-      'I took a leap and enrolled myself in Lighthouse Labs Full-Stack Web Development program. On top of my 40-hour work week, I carved out 30+ hours each week to pursued this interest which had always been in the back of my mind.'
+    date: 'JUNE 4, 2020',
+    title: 'University of Toronto, HBSc.',
+    description: `I graduated with Honors Bachelors of Science degree from the University of Toronto in 2020, with a major in Geographic Information Systems and minors in Economics and Business.`
   },
   {
     id: 5,
-    date: 'APRIL 26, 2024',
-    title: 'Web Development Diploma',
-    description:
-      'This is one of my proudest achievements. Within 8 intense months, I had made incredible progress in my understanding of the important tools, frameworks, and concepts in Web Development. I graduated with the ability to create web applications, and the skills to continue learning independently.'
+    date: 'MAY 20, 2017',
+    title: 'First Commit',
+    description: `Ever since I pushed my first commit to Github in May of 2017, part of me had always wanted to be able to build visually appealing applications that appear simple, but abstracts the layer of complexity for the end user. `
   },
 ];
 

c229a6106868fd6b339777e80d23aef07068eb5c
Gloria Lim <glorialimartt@gmail.com>
2024-05-04 22:12:32 -0400

    Add HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import theme from './theme';
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/index.css'
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <HashRouter>
     <ChakraProvider theme={theme} >
       <App />
     </ChakraProvider>
+    </HashRouter>
   </React.StrictMode>,
 )

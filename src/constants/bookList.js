9acd19f7663989d5149a9a0f7ac1b054f806989d
Gloria Lim <glorialimartt@gmail.com>
2024-05-27 23:06:26 +0800

    Refactor book list

---
+++ src/constants/bookList.js
@@ -0,0 +1,18 @@
+export const bookList = [
+  {
+    title: "The Clean Coder",
+    author: "Robert C. Martin"
+  },
+  {
+    title: "Laws of UX",
+    author: "Jon Yablonski"
+  },
+  {
+    title: "Modern Software Engineering",
+    author: "David Farley"
+  },
+  // {
+  //   title: "",
+  //   author: ""
+  // },
+]

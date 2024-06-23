7ca49b1646e25c7501201c7654fe6b0d4b70e95f
Gloria Lim <glorialimartt@gmail.com>
2024-06-22 23:15:35 -0400

    Refactor to use AnimatedMotionDiv

--- src/components/Pages/Home/Quote.tsx
+++ src/components/Pages/Home/Quote.tsx
@@ -1,6 +1,6 @@
 import { Center, useBreakpointValue } from "@chakra-ui/react";
-import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import { AnimatedMotionDiv } from "../../Motion";
 
 function Quote() {
   const headingFontSize = useBreakpointValue({ base: "0.5em", md: "1.1em" });
@@ -25,16 +25,12 @@ function Quote() {
       fontSize={headingFontSize}
       textShadow= '2px 2px 4px rgba(0, 0, 0, 0.5)'
     >
-      <motion.div
-        initial={{ opacity: 0, y: 20 }} 
-        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }} 
-        transition={{ duration: 0.75 }}
-      >
+      <AnimatedMotionDiv inView={inView} transitionDelay={0.5}>
         <h1 style={{ fontWeight: "bold" }}>The art of programming is the art of organizing complexity.</h1>
         <h2 style={{ fontWeight: "regular", marginTop: "2em", fontSize: "1.8em"}}>
           -Edsger W. Dijkstra, "Notes On Structured Programming"
         </h2>
-      </motion.div>
+      </AnimatedMotionDiv>
     </Center>
   );
 }

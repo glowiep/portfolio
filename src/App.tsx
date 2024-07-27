0b5e13c32827cc33b8f4c7ffc45a4b1464d7832f
Gloria Lim <glorialimartt@gmail.com>
2024-07-27 18:02:33 -0400

    Update console info

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,21 @@ const AppRoutes = () => {
  )
 }
 
-console.log('%c Welcome to my portfolio 🐥👋', 'font-weight: bold; font-size: 1.5rem;color: gold;border-radius: 4px; margin: 1rem; padding: 0.15rem; text-shadow: 3px 2px 2px #0a0121; background: gray; border: 5px solid purple');
+console.info("%c Welcome to Gloria's portfolio 🐥👋", 'font-weight: bold; font-size: 1.5rem;color: gold;border-radius: 4px; margin: 1rem; padding: 0.15rem; text-shadow: 3px 2px 2px #0a0121; background: gray; border: 5px solid purple');
+console.info(`\n%c🚀 Resume 🚀%c\nhttps://bit.ly/gloria_lim %c`,
+    "color:white; background: purple; font-size:1.5rem; margin: 1rem; padding: 0.15rem auto; font-family: Rockwell, Tahoma, 'Trebuchet MS', Helvetica; border: 2px solid #ceb73f; border-radius: 4px; font-weight: bold; text-shadow: 1px 1px 1px #000000bf;",
+    'font-weight: bold; font-size: 1rem;color: #ceb73f; margin: 1rem; padding: 0.15rem',
+    "color: #ceb73f; font-size: 0.75rem; font-family: Tahoma, 'Trebuchet MS', Helvetica;",
+)
+
+setTimeout(() => {
+  console.info(
+    `\n%c⚠️ Before You Go ⚠️%c \nConnect with me on LinkedIn! \nhttps://www.linkedin.com/in/glowiep %c`,
+    "color:#ceb73f; background: #ceb73f33; font-size:1.5rem; margin: 1rem; padding: 0.15rem auto; font-family: Rockwell, Tahoma, 'Trebuchet MS', Helvetica; border: 2px solid #ceb73f; border-radius: 4px; font-weight: bold; text-shadow: 1px 1px 1px #000000bf;",
+    'font-weight: bold; font-size: 1rem;color: #ceb73f; margin: 1rem; padding: 0.15rem',
+    "color: #ceb73f; font-size: 0.75rem; font-family: Tahoma, 'Trebuchet MS', Helvetica;",
+  );
+}, 7000)
 
 function App() {  
   return (
